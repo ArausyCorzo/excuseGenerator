4bb6b88966b4500012f59d0e4f3ba9f791e83e56
ArausyCorzo <90804785+ArausyCorzo@users.noreply.github.com>
2021-10-31 17:01:55 -0400

    Aqui estan mis excusas ;)

---
+++ index.js
@@ -0,0 +1,19 @@
+let who = ['The dog','My grandma','His turtle','My bird'];
+let action = ['ate','peed','crushed','broke'];
+let what = ['my homework', 'the keys', 'the car'];
+let when = ['before the class','right on time','when I finished','during my lunch','while I was praying'];
+
+
+let random1 = Math.floor(Math.random() * who.length);
+let random2 = Math.floor(Math.random() * action.length);
+let random3 = Math.floor(Math.random() * what.length);
+let random4 = Math.floor(Math.random() * when.length);
+
+function randomfrase(){
+    document.getElementById("fraseRandom").innerHTML = who[random1] + ' ' + action[random2] + ' ' + what[random3] + ' ' + when[random4];
+}
+window.onload = randomfrase;
+
+
+
+
